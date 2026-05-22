53336da674def8da24516cc450061968336555a4
Bernie <133473055+bernielaird@users.noreply.github.com>
2026-05-22 16:57:52 -0500

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        res = s + "1"
+        if len(t) > len(s):
+            return ""
+        tHash = {}
+        sHash = {}
+        for i in t:
+            tHash[i] = tHash.get(i, 0) + 1
+        
+        l, r = 0,0
+        while r < len(s):
+            temp = True
+            sHash[s[r]] = sHash.get(s[r], 0) + 1
+            while temp:
+                for j in tHash.keys():
+                    if tHash[j] > sHash.get(j, 0):
+                        temp = False
+                if temp:
+                    if len(s[l:r]) < len(res):
+                        res = s[l:r+1]
+                    sHash[s[l]] = sHash[s[l]] - 1
+                    if sHash[s[l]] == 0:
+                        sHash.pop(s[l])
+                    l += 1
+            r += 1
+        if res == (s + "1"):
+            return ""
+        return res
